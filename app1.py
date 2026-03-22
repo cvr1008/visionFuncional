52de6eb03990b76cae06361d767f09c88361abb3
Cris <cvr1008@alu.ubu.es>
2026-03-22 22:54:44 +0100

    cambios

--- app1.py
+++ app1.py
@@ -41,48 +41,63 @@ pesos_preguntas = {
 }
 
 preguntas = [
-    "1. 📖 Leer un periódico o un libro.",
-    "2. 📺 Ver la televisión.",
+    "1. Leer un periódico o un libro.",
+    "2. Ver la televisión.",
     "3. Ver los precios cuando hace la compra.",
-    "4. ✒️ Escribir cartas/textos a mano, hacer sudokus/crucigramas, etc.",
-    "5. 💻 Manejar el ordenador (trabajar con word/excel) con fines laborales.",
-    "6. 📱 Manejar el teléfono móvil/ tablet.",
-    "7. 🍳 Cocinar",
+    "4. Escribir cartas/textos a mano, hacer sudokus/crucigramas, etc.",
+    "5. Manejar el ordenador (trabajar con word/excel) con fines laborales.",
+    "6. Manejar el teléfono móvil/ tablet.",
+    "7. Cocinar",
     "8. Reconocer caras de personas por la calle.",
     "9. Leer letreros grandes en la calle, nombres de tiendas, etc.",
     "10. Identificar bordillos/escalones o irregularidades en el suelo.",
-    "11. 🧵 Hacer trabajos manuales finos/precisos (ej. coser, manualidades, carpintería).",
-    "12. 🏌️🏓 Participar en deportes (ej. petanca, tenis/pádel, golf) o ver deporte en vivo.",
-    "13. 🎮 Participar en sus aficiones (jugar a juegos de mesa, cartas, pasear)"
+    "11. Hacer trabajos manuales finos/precisos (ej. coser, manualidades, carpintería).",
+    "12. Participar en deportes (ej. petanca, tenis/pádel, golf) o ver deporte en vivo.",
+    "13. Participar en sus aficiones (jugar a juegos de mesa, cartas, pasear)"
 ]
 
 preguntas_conduccion = [
-    "14. ☀️ Conducir de día.",
-    "15. 🌃 Conducir de noche.",
-    "16. 🌄 Conducir al amanecer y/o atardecer.",
-    "17. 🛑🚸 Distinguir señales de tráfico y carteles en la carretera."
+    "14. Conducir de día.",
+    "15. Conducir de noche.",
+    "16. Conducir al amanecer y/o atardecer.",
+    "17. Distinguir señales de tráfico y carteles en la carretera."
 ]
 
 # DICCIONARIO DE IMÁGENES 
 # Aquí enlazas el texto exacto de la pregunta con su foto.
 imagenes_preguntas = {
-    "1. 📖 Leer un periódico o un libro.": "https://images.unsplash.com/photo-1504711434969-e33886168f5c?w=500&q=80" # Foto de prueba de internet
-    # Cuando quieras añadir más, solo tienes que poner una coma arriba y añadir la siguiente línea, por ejemplo:
-    # "2. 📺 Ver la televisión.": "foto_tele.jpg"
+    "1. Leer un periódico o un libro.": "images/periodico.webp",
+    "2. Ver la televisión.": "images/tele.jpg",
+    "3. Ver los precios cuando hace la compra.": "images/compra.jpg",
+    "4. Escribir cartas/textos a mano, hacer sudokus/crucigramas, etc.": "images/escribir.jpg",
+    "5. Manejar el ordenador (trabajar con word/excel) con fines laborales.": "images/ordenador.jpg",
+    "6. Manejar el teléfono móvil/ tablet.": "images/movil.webp",
+    "7. Cocinar":"images/cocinar.jpeg",
+    "8. Reconocer caras de personas por la calle.": "images/encontrarse_gente.jpeg",
+    "9. Leer letreros grandes en la calle, nombres de tiendas, etc.": "images/letreros_calle.jpg",
+    "10. Identificar bordillos/escalones o irregularidades en el suelo.": "images/adoquin.jpg",
+    "11. Hacer trabajos manuales finos/precisos (ej. coser, manualidades, carpintería).": "images/destornillador.webp",
+    "12. Participar en deportes (ej. petanca, tenis/pádel, golf) o ver deporte en vivo.": "images/golf.avif",
+    "13. Participar en sus aficiones (jugar a juegos de mesa, cartas, pasear)": "images/cartas.jpg",
+    "14. Conducir de día.": "images/conducir_de_dia.webp",
+    "15. Conducir de noche.": "images/conducir_de_noche.jpg",
+    "16. Conducir al amanecer y/o atardecer.": "images/conducir_amanecer.avif",
+    "17. Distinguir señales de tráfico y carteles en la carretera.": "images/señales_trafico.jpg"
+
 }
 
-# --- CABECERA FIJA ---
-st.title("👁️ Cuestionario de Visión Funcional")
-st.write("---")
 
 # ==========================================
 # PANTALLA 0: Bienvenida (-1)
 # ==========================================
 if st.session_state.indice_actual == -1:
+    st.title("👁️ Cuestionario de Visión Funcional")
+    st.write("---")
     st.markdown("""
     Este breve cuestionario ayuda a su oftalmólogo a entender cómo afecta su pérdida de visión a su vida diaria (con sus gafas o lentillas en caso de necesitarlas). 
                 
-    Por favor, responda con sinceridad a las siguientes preguntas. Si no realiza alguna actividad por motivos que no tienen que ver con su vista, marque la última opción.
+    Por favor, responda con sinceridad a las siguientes preguntas. 
+    Si no realiza alguna de las siguientes actividades, marque la última opción.
     """)
     st.write("---")
     st.button("Comenzar cuestionario ➡️", on_click=avanzar, use_container_width=True)
